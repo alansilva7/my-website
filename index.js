2aede6d2fba3c0ad870d13641bf5509d40f39ec5
alansilva7 <alan.scaje@gmail.com>
2022-10-24 09:06:11 -0300

    submit

--- index.js
+++ index.js
@@ -29,22 +29,3 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
     headerHamMenuCloseBtn.classList.add('d-none');
   });
 }
-
-// ---
-const headerLogoConatiner = document.querySelector('.header__logo-container');
-
-headerLogoConatiner.addEventListener('click', () => {
-  location.href = 'index.html';
-});
-
-const modalBtn = document.querySelector('.modal-btn');
-const modal = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-btn');
-
-modalBtn.addEventListener('click', () => {
-  modal.classList.add('open-modal');
-});
-
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('open-modal');
-});
